f2a321a5562a392cb36fedeebcf5aec55c1d203e
koefoeden <koefoeden@gmail.com>
2020-10-14 21:08:19 +0200

    new commit

--- src/automatic_testing.py
+++ src/automatic_testing.py
@@ -5,10 +5,11 @@ import re
 import time
 import sys
 import multiprocessing
+from psutil import cpu_count
 
 
 ########################
-# CONFIG FILE TO BE USED FOR SIMULATION
+# CONFIG FILE TO BE USED FOR SIMULATION IF CALLED WITHOUT COMMAND LINE ARGUMENT
 cfg_file = '../configs/automatic_testing/my_config.ini'
 ########################
 # OUTPUT FILE FOR SIMULATION RESULTS
@@ -22,22 +23,20 @@ class Tester:
         self.cfg_file_name = cfg_file_param
         self.config_parser = configparser.ConfigParser()
         self.config_parser.read(self.cfg_file_name)
+        self.sim_specific_vars = self.get_sim_specific_vars()
         self.multicore_mode = self.config_parser['AUTO_TESTING'].getboolean("multi-core_mode")
         self.regex_range = r"([0-9]+)[^0-9]*([0-9]*)[^0-9]*([0-9]*)"
 
-        # set config values
+        # load configs file into nested dict of value lists
         self.config_dict = {}
-
-        # load configs into dict of values list
         for section in self.config_parser.sections():
             self.config_dict[section] = {}
             for (var_name, val) in self.config_parser.items(section):
                 if val not in ['True', 'False']:
                     self.config_dict[section][var_name] = self.get_config_values(self.config_parser[str(section)][str(var_name)])
 
+        # Create file for simulation results with header if not exists
         self.create_results_file()
-
-        self.num_of_configs = self.get_number_of_configs()
 
         # Ask for confirmation and run if positive
         if self.confirm_test_prompt():
@@ -57,18 +56,26 @@ class Tester:
         else:
             return [int(min_val)]
 
+    def get_sim_specific_vars(self):
+        vars = []
+        for section in self.config_parser.sections():
+            for (key, val) in self.config_parser.items(section):
+                vars.append(key)
+
+        to_be_removed = ['repetitions', 'multi-core_mode']
+        for element in to_be_removed:
+            vars.remove(element)
+
+        to_be_added = ['avg_speed_mouse', 'avg_speed_owl', "m_alive", "o_alive", "sim ver"]
+        vars.extend(to_be_added)
+
+        return vars
+
     def create_results_file(self):
         try:
             with open(results_file, 'x', newline='') as csv_file:
-                header = []
-                for section in self.config_parser.sections():
-                    for (key, val) in self.config_parser.items(section):
-                        header.append(key)
-                header.remove(['repetitions', 'multi-core_mode'])
-                header.extend(['avg_speed_mouse', 'avg_speed_owl', 'sim ver'])
-
                 writer = csv.writer(csv_file, delimiter=',')
-                writer.writerow(header)
+                writer.writerow(self.sim_specific_vars)
 
         except FileExistsError:
             pass
@@ -82,63 +89,81 @@ class Tester:
         return num_of_configs
 
     def confirm_test_prompt(self):
+        # Get required numbers for prompt
+        self.num_of_configs = self.get_number_of_configs()
         num_of_simulations = self.num_of_configs*self.config_dict['AUTO_TESTING']['repetitions'][0]
         single_run_time = self.run_simulations(single_run=True)
-        answer = input(f"You have selected {num_of_simulations}"
+        num_cores = cpu_count(logical=False)
+
+        # Ask for input
+        if self.multicore_mode:
+            answer = input(f"You have selected {num_of_simulations}"
                        f" simulations to be run at {self.config_dict['AUTO_TESTING']['ticks'][0]} ticks each, based on"
                        f" {self.num_of_configs} different configurations.\nThis will take approximately"
-                       f" {round(single_run_time*num_of_simulations/60,2)} minutes to perform.\n\nDo you wish to continue? y/n:")
+                       f" {round((single_run_time*num_of_simulations/60)/num_cores,2)} minutes to perform on {num_cores} cores.\n\nDo you wish to continue? y/n:")
+        else:
+            answer = input(f"You have selected {num_of_simulations}"
+                           f" simulations to be run at {self.config_dict['AUTO_TESTING']['ticks'][0]} ticks each, based on"
+                           f" {self.num_of_configs} different configurations.\nThis will take approximately"
+                           f" {round((single_run_time * num_of_simulations / 60), 2)} minutes to perform using the single core mode.\n\nDo you wish to continue? y/n:")
+
         return answer == 'y'
 
     def run_1_rep(self):
         pass
 
     def run_simulations(self, single_run=False):
-        processes = []
-        if not single_run:
-            print('\nRunning simulations... Can be stopped at any time.', end='')
+        # TODO: Does this ruin multi-core processing? Check it
+        with open(results_file, 'a+', newline='') as csv_file:
+            writer = csv.writer(csv_file, delimiter=',')
 
-        t0 = time.time()
+            processes = []
+            if not single_run:
+                print('\nRunning simulations... Can be stopped at any time and results will be saved.')
 
-        for i in range(self.config_dict['AUTO_TESTING']['repetitions'][0]):
-            for value_grass_grow_back in self.config_dict['ENVIRONMENT']['grass_grow_back']:
-                self.config_parser.set("ENVIRONMENT", "grass_grow_back", str(value_grass_grow_back))
+            t0 = time.time()
 
-                for m_value_hunger in self.config_dict['MICE']['m_die_of_hunger']:
-                    self.config_parser.set("MICE", "m_die_of_hunger", str(m_value_hunger))
+            for i in range(self.config_dict['AUTO_TESTING']['repetitions'][0]):
+                if not single_run:
+                    print(f"Simulating repetition {i+1} of {self.config_dict['AUTO_TESTING']['repetitions'][0]} repetitions for given configs.")
+                for value_grass_grow_back in self.config_dict['ENVIRONMENT']['grass_grow_back']:
+                    self.config_parser.set("ENVIRONMENT", "grass_grow_back", str(value_grass_grow_back))
 
-                    for m_value_preg_time in self.config_dict['MICE']['m_preg_time']:
-                        self.config_parser.set("MICE", "m_preg_time", str(m_value_preg_time))
+                    for m_value_hunger in self.config_dict['MICE']['m_die_of_hunger']:
+                        self.config_parser.set("MICE", "m_die_of_hunger", str(m_value_hunger))
 
-                        for m_value_number in self.config_dict['MICE']['m_number']:
-                            self.config_parser.set("MICE", "m_number", str(m_value_number))
+                        for m_value_preg_time in self.config_dict['MICE']['m_preg_time']:
+                            self.config_parser.set("MICE", "m_preg_time", str(m_value_preg_time))
 
-                            for o_value_hunger in self.config_dict['OWLS']['o_die_of_hunger']:
-                                self.config_parser.set("OWLS", "o_die_of_hunger", str(o_value_hunger))
+                            for m_value_number in self.config_dict['MICE']['m_number']:
+                                self.config_parser.set("MICE", "m_number", str(m_value_number))
 
-                                for o_value_preg_time in self.config_dict['OWLS']['o_preg_time']:
-                                    self.config_parser.set("OWLS", "o_preg_time", str(o_value_preg_time))
+                                for o_value_hunger in self.config_dict['OWLS']['o_die_of_hunger']:
+                                    self.config_parser.set("OWLS", "o_die_of_hunger", str(o_value_hunger))
 
-                                    for o_value_number in self.config_dict['OWLS']['o_number']:
-                                        self.config_parser.set("OWLS", "o_number", str(o_value_number))
+                                    for o_value_preg_time in self.config_dict['OWLS']['o_preg_time']:
+                                        self.config_parser.set("OWLS", "o_preg_time", str(o_value_preg_time))
 
-                                        for rand_variance_trait_value in self.config_dict['INHERITANCE']['rand_variance_trait']:
-                                            self.config_parser.set("INHERITANCE", "rand_variance_trait", str(rand_variance_trait_value))
+                                        for o_value_number in self.config_dict['OWLS']['o_number']:
+                                            self.config_parser.set("OWLS", "o_number", str(o_value_number))
 
-                                            if single_run:
-                                                self.run_single_simulation()
-                                                t1 = time.time()
-                                                return t1 - t0
+                                            for rand_variance_trait_value in self.config_dict['INHERITANCE']['rand_variance_trait']:
+                                                self.config_parser.set("INHERITANCE", "rand_variance_trait", str(rand_variance_trait_value))
 
-                                            else:
-                                                # MULTICORE ENABLED
-                                                if self.multicore_mode:
-                                                    p = multiprocessing.Process(target=self.run_single_simulation)
-                                                    processes.append(p)
-                                                    p.start()
+                                                if single_run:
+                                                    self.run_single_simulation(csv_file, writer)
+                                                    t1 = time.time()
+                                                    return t1 - t0
 
                                                 else:
-                                                    self.run_single_simulation()
+                                                    # MULTI-CORE ENABLED
+                                                    if self.multicore_mode:
+                                                        p = multiprocessing.Process(target=self.run_single_simulation(csv_file, writer))
+                                                        processes.append(p)
+                                                        p.start()
+
+                                                    else:
+                                                        self.run_single_simulation(csv_file, writer)
 
         for process in processes:
             process.join()
@@ -147,26 +172,28 @@ class Tester:
         print(f'\n\nTesting completed in {round((full_simulation_time - t0) / 60, 2)} minutes.\n')
         print(f'Results appended to {results_file}.\n')
 
-    def run_single_simulation(self):
-        # get specific configuration
-        row_data = []
+    def run_single_simulation(self, csv_file, writer):
+        # copy specific sim configuration into list
+        sim_data = []
         for section in self.config_parser.sections():
             for (key, val) in self.config_parser.items(section):
-                if not key == 'repetitions':
-                    row_data.append(val)
+                if key in self.sim_specific_vars:
+                    sim_data.append(val)
 
         # create environment and simulate number of ticks
         object_environment = environment.Environment(self.config_parser)
         object_environment.multiple_ticks(self.config_dict['AUTO_TESTING']['ticks'][0])
 
-        # add average speed and simulation version to row data
-        row_data.extend(object_environment.average_speed())
-        row_data.append(object_environment.sim_version)
+        # add average speed, mice alive and owls alive and simulation version to row data
+        sim_data.extend(object_environment.average_speed())
+        sim_data.extend([object_environment.mice_alive, object_environment.owls_alive])
+        sim_data.append(object_environment.sim_version)
 
         # write full row data into file
-        with open(results_file, 'a+', newline='') as csv_file:
-            writer = csv.writer(csv_file, delimiter=',')
-            writer.writerow(row_data)
+        # TODO: Old implementation commented out
+        #with open(results_file, 'a+', newline='') as csv_file:
+        #writer = csv.writer(csv_file, delimiter=',')
+        writer.writerow(sim_data)
 
 if __name__=='__main__':
     try:
--- src/data_grouper.py
+++ src/data_grouper.py
@@ -4,17 +4,31 @@ import pandas as pd
 class Grouper:
     def __init__(self, data_file):
         # read data
-        self.df = pd.read_csv(data_file)
-        self.make_grouped_results()
+        self.df_results = pd.read_csv(data_file)
+        self.df_grouped_results = self.make_grouped_results()
+        self.df_grouped_results.to_csv('../results/grouped_results.csv', index=False)
 
     def make_grouped_results(self):
         # determine groupings
-        column_list = list(self.df)
+        column_list = list(self.df_results)
         remove_list = ['avg_speed_mouse', 'avg_speed_owl']
         group_by_list = [x for x in column_list if x not in remove_list]
 
         # make group object
-        df_config_groups = self.df.groupby(group_by_list)
+        df_config_groups = self.df_results.groupby(group_by_list)
+
+        """
+        # Get average speed, and non-NA and full count within each group.
+        #self.df_results[['avg_speed_mouse', 'avg_speed_owl']] = df_config_groups[['avg_speed_mouse', 'avg_speed_owl']].mean()
+        #self.df_results['non_NA_count_mice'] = df_config_groups['avg_speed_mouse'].count()
+        #self.df_results['non_NA_count_owls'] = df_config_groups['avg_speed_owl'].count()
+        #self.df_results['repetitions'] = df_config_groups['avg_speed_mouse'].size()
+
+        #self.df_results['non_NA_fraction_mice'] = self.df_results['non_NA_count_mice'] / self.df_results['repetitions']
+        #self.df_results['non_NA_fraction_owls'] = self.df_results['non_NA_count_owls'] / self.df_results['repetitions']
+
+        #df_final_rounded = self.df_results.round(2)
+        """
 
         # Get average speed, and non-NA and full count within each group.
         df_average_speeds = df_config_groups[['avg_speed_mouse', 'avg_speed_owl']].mean()
@@ -28,12 +42,11 @@ class Grouper:
         df_merged2 = pd.merge(df_merged, df_non_na_count_owls, on=group_by_list)
 
         df_final = pd.merge(df_merged2, df_full_count, on=group_by_list)
-        df_final['NA_fraction_mice'] = 1 - (df_final['non_NA_count_mice']/df_final['repetitions'])
-        df_final['NA_fraction_owls'] = 1 - (df_final['non_NA_count_owls']/df_final['repetitions'])
+        df_final['non_NA_fraction_mice'] = df_final['non_NA_count_mice']/df_final['repetitions']
+        df_final['non_NA_fraction_owls'] = df_final['non_NA_count_owls']/df_final['repetitions']
 
         df_final_rounded = df_final.round(2)
 
-        df_final_rounded.to_csv('../results/grouped_results.csv', index=False)
 
         return df_final_rounded
 
